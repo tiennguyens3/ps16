b93fa3100a6d5c61c6d76cf052208b6edab4c2ab
tiennguyens3 <tien.nguyen@s3corp.com.vn>
2021-01-26 11:26:52 +0700

    initialize

---
+++ controllers/front/payment.php
@@ -0,0 +1,100 @@
+<?php
+/*
+* 2007-2015 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author PrestaShop SA <contact@prestashop.com>
+*  @copyright  2007-2015 PrestaShop SA
+*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
+/**
+ * @since 1.5.0
+ */
+class Waave_PgPaymentModuleFrontController extends ModuleFrontController
+{
+    const PROD_URL = 'https://pg.getwaave.co/waavepay/checkout';
+    const SANDBOX_URL = 'https://staging-pg.getwaave.co/waavepay/checkout';
+
+    /**
+     * @see FrontController::postProcess()
+     */
+    public function postProcess()
+    {
+        $cart = $this->context->cart;
+        if ($cart->id_customer == 0 || $cart->id_address_delivery == 0 || $cart->id_address_invoice == 0 || !$this->module->active) {
+            Tools::redirect('index.php?controller=order&step=1');
+        }
+
+        // Check that this payment option is still available in case the customer changed his address just before the end of the checkout process
+        $authorized = false;
+        foreach (Module::getPaymentModules() as $module) {
+            if ($module['name'] == 'waave_pg') {
+                $authorized = true;
+                break;
+            }
+        }
+
+        if (!$authorized) {
+            die($this->module->l('This payment method is not available.', 'validation'));
+        }
+
+        $waaveSandbox   = Configuration::get('WAAVE_SANDBOX');
+        $actionUrl = self::PROD_URL;
+        if ($waaveSandbox) {
+            $actionUrl = self::SANDBOX_URL;
+        }
+
+        $accessKey = Configuration::get('ACCESS_KEY');
+        $venueId = Configuration::get('VENUE_ID');
+
+        $request = array(
+            'id_cart' => $cart->id,
+            'id_module' => $this->module->id,
+            'key' => $this->context->customer->secure_key
+        );
+        $returnUrl = $this->context->link->getPageLink('order-confirmation', true, null, $request);
+        $callbackUrl = $this->context->link->getModuleLink($this->module->name, 'validation', $request, true);
+        $cancelUrl = $this->context->link->getPageLink('order', true, null, ['step' => 3]);
+
+        $amount = $cart->getOrderTotal(true, Cart::BOTH);
+        $referenceId = $cart->id;
+
+        $this->addJqueryPlugin('fancybox');
+        $this->registerJavascript(sha1('modules/waave_pg/views/js/waave_pg.js'), 'modules/waave_pg/views/js/waave_pg.js', ['priority' => 100]);
+
+        $vars = array(
+            'accessKey' => $accessKey,
+            'venueId' => $venueId,
+            'referenceId' => $referenceId,
+            'amount' => $amount,
+            'cancelUrl' => $cancelUrl,
+            'returnUrl' => $returnUrl,
+            'callbackUrl' => $callbackUrl,
+            'actionUrl' => $actionUrl
+        );
+
+        PrestaShopLogger::addLog('Waave - Begin checkout', 1, null, null, null, true);
+        PrestaShopLogger::addLog('Data: ' . json_encode($vars), 1, null, null, null, true);
+
+        $this->context->smarty->assign($vars);
+
+        $this->setTemplate('module:waave_pg/views/templates/front/payment_return.tpl');
+    }
+}
---
+++ controllers/front/validation.php
@@ -0,0 +1,134 @@
+<?php
+/*
+* 2007-2015 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author PrestaShop SA <contact@prestashop.com>
+*  @copyright  2007-2015 PrestaShop SA
+*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
+/**
+ * @since 1.5.0
+ */
+class Waave_PgValidationModuleFrontController extends ModuleFrontController
+{
+    /**
+     * @see FrontController::postProcess()
+     */
+    public function postProcess()
+    {
+        $cartId = (int)Tools::getValue('id_cart');
+        $row = DB::getInstance()->getRow('SELECT * FROM ' . _DB_PREFIX_ . 'cart WHERE id_cart = ' . $cartId);
+
+        if ($row['id_customer'] == 0 || $row['id_address_delivery'] == 0 || $row['id_address_invoice'] == 0 || !$this->module->active) {
+            header("HTTP/1.0 404 Not Found");
+            die($this->module->l('Error, cart not found.', 'validation'));
+        }
+
+        $cart = new Cart($cartId);
+        $this->context->cart = $cart;
+
+        $customer = new Customer($cart->id_customer);
+        if (!Validate::isLoadedObject($customer)) {
+            header("HTTP/1.0 404 Not Found");
+            die($this->module->l('Error, customer not found.', 'validation'));
+        }
+
+        PrestaShopLogger::addLog('Waave - process return url', 1, null, null, null, true);
+        PrestaShopLogger::addLog('Validate order', 1, null, null, null, true);
+
+        $json = file_get_contents('php://input');
+        $data = json_decode($json, true);
+
+        if ($data['reference_id'] != $cart->id) {
+            header("HTTP/1.0 404 Not Found");
+            die($this->module->l('Error, response data is invalid.', 'validation'));
+        }
+
+        $request = array(
+            'id_cart' => $cart->id,
+            'id_module' => $this->module->id,
+            'key' => $customer->secure_key
+        );
+
+        $callbackUrl = $this->context->link->getModuleLink($this->module->name, 'validation', $request, true);
+
+        $valid = $this->validateSignature($data, $callbackUrl);
+        if (!$valid) {
+            header("HTTP/1.0 500 Error");
+            die('Error, signature is invalid.');
+        }
+
+        $total = (float)$cart->getOrderTotal(true, Cart::BOTH);
+        if ($total != $data['amount']) {
+            header("HTTP/1.0 500 Error");
+            die('Error, amount is invalid.');
+        }
+
+        $currency = $this->context->currency;
+
+        $status = $data['status'];
+        $orderState = Configuration::get('PS_OS_ERROR');
+        if ( 'completed' === $status ) {
+            $orderState = Configuration::get('PS_OS_PAYMENT');
+        } elseif ( 'pending' === $status ) {
+            $orderState = Configuration::get('PS_OS_CHEQUE');
+        } elseif ( 'cancelled' === $status ) {
+            $orderState = Configuration::get('PS_OS_CANCELED');
+        }
+
+        $valid = $this->module->validateOrder($cart->id, $orderState, $total, $this->module->displayName, NULL, [], (int)$currency->id, false, $customer->secure_key);
+
+        if (!$valid) {
+            header("HTTP/1.0 500 Error");
+            die('Error, order is invalid.');
+        }
+
+        PrestaShopLogger::addLog('Waave - request validation is done', 1, null, null, null, true);
+
+        die('OK-PRESTASHOP');
+    }
+
+    /**
+     * Validate signature
+     * 
+     * @param array $data
+     */
+    private function validateSignature($data, $uri) {
+        $secretKey = Configuration::get('PRIVATE_KEY');
+        $body      = json_encode($data);
+
+        $signature       = hash("sha256", $secretKey . $uri . $body);
+        $headerSignature = isset($_SERVER['HTTP_X_API_SIGNATURE']) ? $_SERVER['HTTP_X_API_SIGNATURE'] : '';
+
+        if ($signature === $headerSignature) {
+            PrestaShopLogger::addLog('Signature is valid.');
+            return true;
+        }
+
+        PrestaShopLogger::addLog('Signature is invalid.', 1, null, null, null, true);
+        PrestaShopLogger::addLog('Signature: ' . $signature, 1, null, null, null, true);
+        PrestaShopLogger::addLog('Secret key: ' . $secretKey, 1, null, null, null, true);
+        PrestaShopLogger::addLog('Uri: ' . $uri, 1, null, null, null, true);
+        PrestaShopLogger::addLog('Body: ' . $body, 1, null, null, null, true);
+
+        return false;
+    }
+}
